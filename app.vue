66f21dcd46fa1de8b2745c9ebbc6472275e755eb
RobinVanDutch <robinvandutch@mail.ru>
2023-01-31 15:09:02 +0700

    PHPStorm test

--- app.vue
+++ app.vue
@@ -2,4 +2,20 @@
   <div>
     Hello world
   </div>
+  <button @click="plus">Plus</button>
+  <p>{{ count }}</p>
+  <button @click="minus">Minus</button>
+  
 </template>
+
+<script setup>
+  const count = ref(0);
+
+  function plus() {
+    count.value++;
+  }
+
+  function minus() {
+    count.value--;
+  }
+</script>
